f1f71b3fc8b2342a02cda762f04dbfc98ca26a00
arthurpph <arthurpiana13@gmail.com>
2023-11-03 01:52:58 -0300

    added music duration and current music time on the player

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import '../assets/styles/CustomToast.css'
-import '../assets/styles/Login.css'
+import '../assets/styles/CustomToast.css';
+import '../assets/styles/Login.css';
 
 function Login() {
     const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
@@ -20,7 +20,7 @@ function Login() {
                 <label htmlFor="password">Insira sua senha</label>
                 <input type="password" name="password"/>
                 <button type="submit">LOGIN</button>
-                <p>Não tem uma conta? <Link to="/register" style={{textDecoration: 'none'}}>Registre-se</Link></p>         
+                <p>Não tem uma conta? <Link to="/register" style={{ textDecoration: 'none' }}>Registre-se</Link></p>         
             </form>
         </div>
     );
