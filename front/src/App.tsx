3b88dad7537d25031f78ca32f874c97f0e1145a2
arthurpph <arthurpiana13@gmail.com>
2023-11-11 00:20:31 -0300

    added api request each 10 minutes to avoid free plan inactivity

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import { useEffect } from 'react';
+import axios from 'axios';
 import Container from './components/Container'
 import Main from './pages/Main'
 import Login from './pages/Login'
@@ -7,6 +9,14 @@ import PageNotFound from './pages/PageNotFound'
 import Register from './pages/Register'
 
 function App() {
+  const request = async () => {
+    await axios.get('https://tabely.onrender.com/users');
+  }
+
+  useEffect(() => {
+    setInterval(request, 300000);
+  })
+
   return (
     <Router>
       <Container>
