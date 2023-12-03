bf77bedec6ce74f9e7c56087dbb8f8c2aeb6b952
Arthur <62454837+arthurpph@users.noreply.github.com>
2023-12-03 11:05:33 -0300

    Update Main.tsx

--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -1,15 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import { getCookie } from "../helpers/getCookie";
 import MainComponent from "../components/MainComponent";
 import Navbar from "../components/Navbar";
 import { useEffect } from "react";
 
 function Main() {
-    const navigate = useNavigate();
-
     useEffect(() => {
         if(!getCookie('loginToken')) {
-            navigate('/welcome');
+            window.location.href = "/welcome"
         }
     }, [])
 
