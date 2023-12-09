1e4913f54b41dc8de6e9c949b49949166da87d64
arthurpph <arthurpiana13@gmail.com>
2023-12-09 17:19:41 -0300

    2 attempt to fix mobile problem

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -46,7 +46,8 @@ function Login() {
                 return;
             }
 
-            cookies.set('loginToken', data.accessToken);
+            cookies.set('loginToken', data.accessToken, { HttpOnly: false });
+            cookies.set('isLogged', 'true', { HttpOnly: false });
             
             toast.success(data.response);
             window.location.href = '/';
--- front/src/pages/Main.tsx
+++ front/src/pages/Main.tsx
@@ -1,22 +1,9 @@
 import { useEffect } from "react";
+import { getCookie } from "../helpers/getCookie";
 import MainComponent from "../components/MainComponent";
 import Navbar from "../components/Navbar";
 
 function Main() {
-    const getCookie = (name: string) => {
-        const cookieString = document.cookie;
-        const cookies = cookieString.split('; ');
-      
-        for(let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].split('=');
-          if(cookie[0] === name) {
-            return cookie[1];
-          }
-        }
-      
-        return null;
-      }
-
     useEffect(() => {
         if(!getCookie('loginToken')) {
             window.location.href = "/welcome"
