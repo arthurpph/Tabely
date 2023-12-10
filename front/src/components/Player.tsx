9ec17fdb50504348adfece0853c2ac38a07e5e14
arthurpph <arthurpiana13@gmail.com>
2023-12-09 23:06:16 -0300

    attempt 4

--- front/src/components/Player.tsx
+++ front/src/components/Player.tsx
@@ -237,11 +237,14 @@ function Player() {
         setMusicDuration(music.duration);
         setPlayImageButton(PauseButton);
         if(!firstMusicSetup) {
-            if(audio?.paused) {
-                setTimeout(() => {
-                    audio?.play();
-                }, 500);
-            }
+            setTimeout(() => {
+                if(audio?.paused) {
+                    setTimeout(() => {
+                        audio?.play();
+                    }, 500);
+                }
+            }, 500);
+            
             audio?.addEventListener('canplaythrough', () => {
                 audio.play();
             });
